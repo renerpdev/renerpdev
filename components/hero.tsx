6a40f4c7e969bad868d2d271e018f50888932fe2
René Ricardo <32307287+renericardoperez@users.noreply.github.com>
2024-04-18 15:17:54 -0300

    add button for downloading CV
    Signed-off-by: René Ricardo <32307287+renericardoperez@users.noreply.github.com>

--- components/hero.tsx
+++ components/hero.tsx
@@ -16,33 +16,46 @@ const Hero = () => {
           I am a Frontend Developer based in Montevideo, Uruguay. If you need a developer who can bring your ideas to
           life, feel free to contact me.
         </p>
-        <m.a
-          href="#contact"
-          whileHover={{ scale: 1.1 }}
-          whileTap={{ scale: 1 }}
-          transition={{ type: "spring", stiffness: 400, damping: 10 }}
-          className={
-            "font-semibold max-w-sm mx-auto md:ml-0 text-lg flex md:inline-flex justify-center items-center bg-white px-8 py-3 rounded-3xl border-none"
-          }>
-          <span className={"mr-2 bg-gradient-to-r from-cyan-950 to-cyan-700 bg-clip-text text-transparent"}>
-            {"Let's connect"}
-          </span>
-          <RocketIcon className="text-cyan-900" />
-        </m.a>
+        <div className={"inline-flex flex-col sm:flex-row gap-4 "}>
+          <m.a
+            href="#contact"
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 1 }}
+            transition={{ type: "spring", stiffness: 400, damping: 10 }}
+            className={
+              "font-semibold max-w-sm mx-auto md:ml-0 text-lg flex md:inline-flex justify-center items-center bg-white px-8 py-3 rounded-3xl border-none"
+            }>
+            <span className={"mr-2 bg-gradient-to-r from-cyan-950 to-cyan-700 bg-clip-text text-transparent"}>
+              {"Let's connect"}
+            </span>
+            <RocketIcon className="text-cyan-900" />
+          </m.a>
+          <m.a
+            href="/renerp-cv.pdf"
+            download
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 1 }}
+            transition={{ type: "spring", stiffness: 400, damping: 10 }}
+            className={
+              "font-semibold max-w-sm mx-auto md:ml-0 text-lg flex md:inline-flex justify-center items-center bg-cyan-600 px-8 py-3 rounded-3xl border-none"
+            }>
+            <span className={"mr-2 text-white"}>{"Download CV"}</span>
+          </m.a>
+        </div>
         <div className={"flex justify-center md:justify-start space-x-2 items-center mt-6"}>
           <a
             href="https://github.com/renerpdev"
             target="_blank"
             rel="noreferrer noopener"
             className={"inline-flex text-white hover:text-cyan-600"}>
-            <GithubIcon />
+            <GithubIcon className={"w-6 h-6"} />
           </a>
           <a
             href="https://www.linkedin.com/in/renerpdev"
             target="_blank"
             rel="noreferrer noopener"
             className={"inline-flex text-white hover:text-cyan-600"}>
-            <LinkedinIcon />
+            <LinkedinIcon className={"w-8 h-8"} />
           </a>
         </div>
       </div>
