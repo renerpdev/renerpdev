8e4335a10bf3e9bbaeb6fb295cb93c6df115a36d
René Ricardo <32307287+renericardoperez@users.noreply.github.com>
2024-04-24 18:48:51 -0300

    fix alignment issue on navbar
    Signed-off-by: René Ricardo <32307287+renericardoperez@users.noreply.github.com>

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -48,7 +48,7 @@ const Navbar = ({ setCursorText, setCursorVariant }: CursorAnimationHandler) => 
       custom={height}
       ref={containerRef}
       className={`${isOpen ? "fixed z-40" : "absolute"} top-0 bottom-0 right-0 w-screen h-screen p-8 flex justify-between items-start`}>
-      <LogoIcon className={"relative z-20 w-8 h-8 text-cyan-600"} />
+      <LogoIcon className={`relative z-20 w-8 h-8 text-cyan-600 ${isOpen ? "hidden" : "block"}`} />
       <m.div className="bg-white absolute top-0 bottom-0 right-0 w-full z-20" variants={sidebar} />
       <div className={"flex justify-start items-end flex-col w-full h-full"}>
         <MenuToggle
