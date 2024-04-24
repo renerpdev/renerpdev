91d09b4ef1b10e6b5098f3ba35cfa41778808c13
René Ricardo <32307287+renericardoperez@users.noreply.github.com>
2024-04-24 10:19:10 -0300

    change app icons and logo
    Signed-off-by: René Ricardo <32307287+renericardoperez@users.noreply.github.com>

--- components/icons/logo.tsx
+++ components/icons/logo.tsx
@@ -5,81 +5,46 @@ export const LogoIcon = ({ className }: { className?: string }) => {
       aria-hidden="true"
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
-      width="125.024"
-      height="202.211"
-      viewBox="0 0 125.024 202.211">
-      <g id="Group_1" data-name="Group 1" transform="translate(-77 -49)">
-        <path
-          id="Path_1"
-          data-name="Path 1"
-          d="M80.609,247.439V61.066l91.639,11.051v81.732l-55.606,13.646,71.979,79.945"
-          transform="translate(6.341 -3.289)"
-          fill="none"
-          stroke="currentColor"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="10"
+      width="175"
+      height="202"
+      viewBox="0 0 175 202">
+      <g id="Group_1" data-name="Group 1" transform="translate(-55 -33)">
+        <rect
+          id="Rectangle_2"
+          data-name="Rectangle 2"
+          width="45"
+          height="200"
+          rx="5"
+          transform="translate(55 35)"
+          fill="#fff"
         />
-        <g
-          id="Ellipse_1"
-          data-name="Ellipse 1"
-          transform="translate(77 49)"
+        <rect
+          id="Rectangle_3"
+          data-name="Rectangle 3"
+          width="45"
+          height="115"
+          rx="5"
+          transform="translate(120 35)"
           fill="#fff"
-          stroke="currentColor"
-          strokeWidth="2">
-          <circle cx="10.5" cy="10.5" r="10.5" stroke="none" />
-          <circle cx="10.5" cy="10.5" r="9.5" fill="none" />
-        </g>
-        <g
-          id="Ellipse_2"
-          data-name="Ellipse 2"
-          transform="translate(166 60)"
+        />
+        <rect
+          id="Rectangle_4"
+          data-name="Rectangle 4"
+          width="45"
+          height="90"
+          rx="5"
+          transform="translate(185 33)"
           fill="#fff"
-          stroke="currentColor"
-          strokeWidth="2">
-          <circle cx="10.5" cy="10.5" r="10.5" stroke="none" />
-          <circle cx="10.5" cy="10.5" r="9.5" fill="none" />
-        </g>
-        <g
-          id="Ellipse_3"
-          data-name="Ellipse 3"
-          transform="translate(111 157)"
+        />
+        <rect
+          id="Rectangle_5"
+          data-name="Rectangle 5"
+          width="45"
+          height="72.927"
+          rx="5"
+          transform="translate(145.006 189.106) rotate(-51)"
           fill="#fff"
-          stroke="currentColor"
-          strokeWidth="2">
-          <ellipse cx="11" cy="10.5" rx="11" ry="10.5" stroke="none" />
-          <ellipse cx="11" cy="10.5" rx="10" ry="9.5" fill="none" />
-        </g>
-        <g
-          id="Ellipse_4"
-          data-name="Ellipse 4"
-          transform="translate(166 139)"
-          fill="#fff"
-          stroke="currentColor"
-          strokeWidth="2">
-          <ellipse cx="10.5" cy="11.5" rx="10.5" ry="11.5" stroke="none" />
-          <ellipse cx="10.5" cy="11.5" rx="9.5" ry="10.5" fill="none" />
-        </g>
-        <g
-          id="Ellipse_5"
-          data-name="Ellipse 5"
-          transform="translate(180 228)"
-          fill="#fff"
-          stroke="currentColor"
-          strokeWidth="2">
-          <circle cx="11" cy="11" r="11" stroke="none" />
-          <circle cx="11" cy="11" r="10" fill="none" />
-        </g>
-        <g
-          id="Ellipse_6"
-          data-name="Ellipse 6"
-          transform="translate(77 228)"
-          fill="#fff"
-          stroke="currentColor"
-          strokeWidth="2">
-          <ellipse cx="10.5" cy="11" rx="10.5" ry="11" stroke="none" />
-          <ellipse cx="10.5" cy="11" rx="9.5" ry="10" fill="none" />
-        </g>
+        />
       </g>
     </svg>
   )
